9738ab1bc632a8dcd0b7415458bb9e78d89f9f5e
nielchvo <nielchvo@gmail.com>
2019-01-16 19:41:47 +0700

    !commit solution

--- Javascript/Algoritm/interview_kit/infiniteString.js
+++ Javascript/Algoritm/interview_kit/infiniteString.js
@@ -1,25 +1,29 @@
-const infiniteString = (n, s) => {
-    // arrange the string
+const infiniteString = (s, n) => {
+    // Arrange the string
     let c = 0, ca = 0;
     let r = n % s.length;
     
-    console.log(r)
-    // get 'a' main
+    // calculate in 'a' the main
     for(let i=0; i<s.length; i++) {
         if(s[i] == 'a')
             c++;
     }
-    // get 'a' rest
+
+    // calculate 'a' the rest
     for(let i=0; i < r; i++) {
         if(s[i] == 'a'){ 
             ca++
         }
     }
     
-    console.log(c * (n / s.length), ca)
-    return 0;
+    // sum 1 line of 'a' then multiplied by the string line minus rest
+    let multipliedStr = c * Math.floor(n / s.length);
+    //console.log(multipliedStr, ca, c, (n/ s.length))
+    
+    return multipliedStr + ca;
 }
 
-
-var result = infiniteString(10, "aba")
+// test case 11 & example 
+//var result = infiniteString(10, "aba")
+var result = infiniteString("epsxyyflvrrrxzvnoenvpegvuonodjoxfwdmcvwctmekpsnamchznsoxaklzjgrqruyzavshfbmuhdwwmpbkwcuomqhiyvuztwvq", 549382313570)
 console.log(result)
