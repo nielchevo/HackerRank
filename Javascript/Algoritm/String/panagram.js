2e67cd947f06514560676951906fe2e08fdf9d51
nielchvo <nielchvo@gmail.com>
2019-02-21 23:16:16 +0700

    ! commit solution

--- Javascript/Algoritm/String/panagram.js
+++ Javascript/Algoritm/String/panagram.js
@@ -1,22 +1,34 @@
 
 function pangrams(s) {
-    var m_map = new Map();
-    
-    for(let i=0; i < 26; i++) {
-        m_map.set(i+97, 1);
-    }
+  // generate alpabeth map
+  var charmap = new Map(); 
+  
+  for(let i=0; i < 26; i++) {
+    charmap.set(i+97, false);
+  }
 
-    for(let i=0; i< s.length; i++) {        
-        let lowerCase = s[i].toLowerCase().charCodeAt();
-        
-        if(m_map.get(lowerCase) != 0) {
-            m_map.set(lowerCase, 0);
-        } 
+  for(let i=0; i< s.length; i++) {       
+    // convert char to ASCII 
+    let asciiNum = s[i].toLowerCase().charCodeAt();
 
-        var testt = m_map.forEach(obj=> {
-            
-        })
+    if(asciiNum >= 97 && asciiNum <= 122){
+      if(charmap.get(asciiNum) == 0) {
+        charmap.set(asciiNum, 1);
+      }
     }
+  }
+
+  //panagram check
+  let isPanagram = true;
+  for(let i=0; i < charmap.size; i++) {
+    if(charmap.get(i+97) == false) {
+      isPanagram = false;
+    }
+  }
+
+  return (isPanagram) ? "pangram": "not pangram";
 }
 
-pangrams("Aabst");
+
+var result = pangrams("We promptly judged antique ivory buckles for the next prize");
+console.log(result);
