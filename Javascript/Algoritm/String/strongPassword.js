c9997d3e83a8017de9dcc020d0c1d2943b1c57eb
nielchvo <nielchvo@gmail.com>
2019-02-23 00:53:52 +0700

    ! commit solution

--- Javascript/Algoritm/String/strongPassword.js
+++ Javascript/Algoritm/String/strongPassword.js
@@ -12,30 +12,32 @@ function minimumNumber(n, s) {
     let lower_case = "abcdefghijklmnopqrstuvwxyz"
     let upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     let special_characters = "!@#$%^&*()-+"
-    let flag = new Array(3).fill(0);
 
-    for(let i in s) {
-        if(charCheck(s[i], lower_case)) {
+    let flag = new Array(4).fill(0);
+    for (let i in s) {
+        if (charCheck(s[i], lower_case)) {
             flag[0] = 1;
-        }else if(charCheck(s[i], upper_case)) {
+        } else if (charCheck(s[i], upper_case)) {
             flag[1] = 1;
-        }else if(charCheck(s[i], special_characters)) {
+        } else if (charCheck(s[i], special_characters)) {
             flag[2] = 1;
-        }else if(charCheck(s[i], numbers)) {
+        } else if (charCheck(s[i], numbers)) {
             flag[3] = 1;
         }
     }
-    console.log(flag)
-    let sum =0;
 
-    for(let i in flag ){ 
-        if(flag[i] == 1){
-            sum+=1;
+    if (n >= 6) { flag[4] = 1; }
+
+    let strong = 0;
+    for (let i in flag) {
+        if (flag[i] == 0) {
+            strong += 1;
         }
     }
 
-    return (n < 6) ? 6 - n : 4 - sum;
+    return (6 - n > strong) ? 6 - n : strong;
 }
-var input= "goxg";
+
+var input= "#HackerRank";
 var result = minimumNumber(input.length, input);
 console.log(result);
