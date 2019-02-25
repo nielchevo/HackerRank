cceb1bf430bfc02cf3b6ca2ab5b7a64c8048f8fb
nielchvo <nielchvo@gmail.com>
2019-02-25 22:11:23 +0700

    ! commit solution (JS)

--- Javascript/Algoritm/String/twoCharacter.js
+++ Javascript/Algoritm/String/twoCharacter.js
@@ -1,26 +1,50 @@
-var input = 'beabeefeab';
 
-/**
- * 
- * @param {String} charset 
- * @returns {Array}
- */
-function removeDuplicate(charset) {
-    let char_set = [];
-    for (let i = 0; i < charset.length; i++) {
-        char_set[charset[i]] = charset[i];
+function twoCharacter(s) {
+  // Remove duplicates char to get pattern
+  let pattern = "";
+  for(let i=0; i < s.length; i++) {
+    if(pattern.indexOf(s[i]) == -1) {
+      pattern += s[i];
     }
-    return char_set;
-}
+  }
+  let max = 0;
+  for(let itr=0; itr < pattern.length; itr++) {
+    for(let itr2=(itr+1); itr2 < pattern.length; itr2++) {
+      let a=' ';
+      let counter= 0;
 
-function twoCharacterSet(input) {
+      // iterate trought input string
+      for(let i in s) {
+        //console.log("%s :: [%s, %s] ", s[i], pattern[itr], pattern[itr2]);
+        if( s[i] == pattern[itr] || s[i] == pattern[itr2]) {
+          //console.log("a: %s | s[i]: %s", a, s[i]);
+          if(a == ' ') {
+            a = s[i];
+            counter++;
+          } else {
+            if(s[i] != a) {
+              counter++;
+              a = s[i];
+            } else {
+              counter = 0; 
+              break;
+            }
+          }
+          //console.log("counter ",counter);
+        }
+      }
+
+      // record longest pattern
+      if( max < counter ) {
+        max = counter;
+      }
+    }
+  }
     
-    var test = removeDuplicate(input);
-    console.log(test.length)
-    for(let i=1; i < 4; i++) {
-       
-       console.log("test");
-    }
+  return max;
 }
 
-twoCharacterSet(input);
+var input = 'beabeefeab';
+var result = twoCharacter(input);
+console.log(result);
+
